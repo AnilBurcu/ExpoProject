96253106944949679cd7e1ed6720e6c6dcdc9245
AnilBurcu <anlbrc95@gmail.com>
2025-04-21 12:32:12 +0300

    first commit

---
+++ src/navigation/RootNavigation.jsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import ProductsScreen from "../screens/ProductsScreen";
+import { SCREENS } from "../utils/rooter";
+
+const Stack = createNativeStackNavigator();
+// obje dağıtma
+const { HOMESCREEN, PRODUCTSCREEN } = SCREENS;
+
+const RootNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={HOMESCREEN} component={HomeScreen} />
+      <Stack.Screen name={PRODUCTSCREEN} component={ProductsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigation;
